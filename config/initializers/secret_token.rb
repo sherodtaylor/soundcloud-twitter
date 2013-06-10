a8beca8b1d0539fa94b620e60a9a99e6671bb76f
Sherod Taylor <sherodtaylor@gmail.com>
2013-06-10 13:07:07 -0700

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundcloudTwitter::Application.config.secret_token = 'ffa4b395e7f8a13c3acd285e593bf9729b4ad2a3b93cf04acb0dd6e6a345cc2d387f58f78d007737951a540b08ed92ecdb1a850200cbcb89362b280ecbdfd9a3'
