8c210ffab0d7713dc6e9d657fcc07f40112555d3
Sherod Taylor <sherodtaylor@gmail.com>
2013-06-13 23:25:51 -0700

    frontend work done

--- app/assets/javascripts/soundcloud.js
+++ app/assets/javascripts/soundcloud.js
@@ -1,71 +1,63 @@
 $(document).ready(function(){
   $('#button').bind("click", function(e){
     e.preventDefault();
-    return false;
-    theCall();
+    $('iframe').remove();
+    $('#player').empty();
+    if ( document.getElementById("sc-widget") == null ){
+      theCall();
+    }
   });
 
   $('#input').bind('click', function(e){
     if (event.keyCode == 13) {
       return false;
-      theCall();
-    }
+      $('iframe').remove();
+      $('#player').empty();
+      if ( document.getElementById("sc-widget") == null ){
+        theCall();
+      }
+    };
   });
-});
 
-theCall(){
-    $('.tweet').remove();
-    $('#sc-widget').remove();
-    if (document.getElementById('sc-widget') == null) {
-    $.ajax({url: "/playlist?q=" + encodeURIComponent($('#input').val())
-          ,method: 'get'
-          ,dataType: 'text'
-          ,success: function(e){
-          console.log(e)
-          $('#player').append('<iframe id="sc-widget" src="https://w.soundcloud.com/player/?url=' + e + '" width="100%" height="100%" scrolling="no" frameborder="no"></iframe>');
-          var widgetIframe = document.getElementById('sc-widget'),
-              widget       = SC.Widget(widgetIframe);
+  function theCall(){
+      if (document.getElementById('sc-widget') == null) {
 
-          widget.bind(SC.Widget.Events.READY, function() {
-            widget.bind(SC.Widget.Events.PLAY, function() {
-              // get information about currently playing sound
-              widget.getCurrentSound(function(currentSound) {
-                console.log('sound ' + currentSound.get('') + 'began to play');
-              });
-            });
-            // get current level of volume
-            widget.getVolume(function(volume) {
-              console.log('current volume value is ' + volume);
-            });
-          }); // widget
+      setInterval(tweetCall, 10000);
+      function tweetCall(){
+      $.ajax({
+        url: "/twitter?q=" + encodeURIComponent($('#input').val())
+       ,method: "get"
+       ,dataType: "json"
+       ,success: function (tweets){
+          for (var i = 0; i < tweets.length; 1 =+ i){
+              var source = $("#tweet-template").html()
+                 ,template = Handlebars.compile(source)
+                 ,context = tweets[i]
+                 ,html = template(context);
 
-          setInterval(tweetCall, 10000);
-
-            function tweetCall(){
-              $.ajax({
-                url: "/twitter?q=" + encodeURIComponent($('#input').val());
-               ,method: "get"
-               ,dataType: "json"
-               ,success: function (tweets){
-                array = tweets
-                for (var i = 0; i < tweets.length; 1 =+ i){
-                  var source   = $("#tweet-template").html();
-                  var template = Handlebars.compile(source);
-                  var context = tweets[i]
-                  var html    = template(context);
-                  $('#tweet-box').prepend(html);
-                }
-               }
-              });
+              $('#tweet-box').prepend(html);
             }
-          } // success
-         ,error: function (data, error){
-           console.log(data);
-           console.log(error);
-         }
-    });}
+        }
+      });
+      };
 
-    else {
-      console.log('did not fire ajax')
-    }
-}
+      $.ajax({url: "/playlist?q=" + encodeURIComponent($('#input').val())
+            ,method: 'get'
+            ,dataType: 'text'
+            ,success: function(e){
+            console.log("I'm Appending Now")
+            $('#player').append('<iframe id="sc-widget" src="https://w.soundcloud.com/player/?url=' + e + '" width="100%" height="100%" scrolling="no" frameborder="no"></iframe>');
+            var widgetIframe = document.getElementById('sc-widget'),
+                widget       = SC.Widget(widgetIframe);
+              }
+           ,error: function (data, error){
+             console.log(data);
+             console.log(error);
+           }});
+      }
+      else {
+        return false
+        console.log('did not fire ajax')
+      }
+}});
+
--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -13,10 +13,9 @@ class TwitterController < ApplicationController
       )
     end
     if last_id != nil
-      json = client.search(q, { :since_id => last_id, :count => 10 })['statuses'].map{ |s| { link: s['user']['profile_background_image_url'], tweet: s['text'] } }
+      json = client.search(q, { :since_id => last_id, :count => 5 })['statuses'].map{ |s| { link: s['user']['profile_background_image_url'], tweet: s['text'] } }
     else
-      test = client.search(q, { :count => 10 })
-      json = client.search(q, { :count => 10 })['statuses'].map{ |s| { pic: s['user']['profile_background_image_url'], tweet: s['text'], date: s['created_at'].strftime("%H:%M%P - %e %b %Y"), handle: s['user']['screen_name'], name: s['user']['name'], id: s['id'].to_s} }
+      json = client.search(q, { :count => 5 })['statuses'].map{ |s| { pic: s['user']['profile_background_image_url'], tweet: s['text'], date: s['created_at'].strftime("%H:%M%P - %e %b %Y"), handle: s['user']['screen_name'], name: s['user']['name'], id: s['id'].to_s} }
     end
     render :json => json.to_json
   end
